e9abb768c286a0c527bc2150d1b609a13334a5c1
aringo <ringo.aaron@gmail.com>
2023-08-14 11:31:24 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,15 +6,12 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Bishop Fox Takeover</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <Header title="Parked Courtesy of Bishop Fox" />
       </main>
 
       <Footer />
